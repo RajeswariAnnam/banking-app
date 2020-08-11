495bd24f512f0c8e896e16e6d1dd33cf95a338bc
AnnamR <Rajeswari.Annam.HCL@kantar.com>
2020-08-11 13:19:16 +0530

    Employee list

---
+++ src/app/empdata.ts
@@ -0,0 +1,10 @@
+import { Emp } from './emp';
+
+export const EMPS: Emp[] = [
+  { id: 1, name: 'User1' },
+  { id: 2, name: 'User2' },
+  { id: 3, name: 'User3' },
+  { id: 4, name: 'User4' }
+  
+  
+];
